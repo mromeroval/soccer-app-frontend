b3bd9614f4ab3f9f10b00a2a2ce60547c55a8721
mromeroval <mauricio.romero@outlook.com>
2020-02-10 17:49:39 +0100

    Implement fetch players function and display on player list

--- src/App.js
+++ src/App.js
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import PlayerList from './Components/Player/PlayerList'
-import PlayerSingle from './Components/Player/PlayerSingle'
-import PlayerForm from './Components/Player/PlayerForm'
+import PlayerList from './Components/Player/PlayerList';
+import PlayerSingle from './Components/Player/PlayerSingle';
+import PlayerForm from './Components/Player/PlayerForm';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col s12">
-          Manu
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+      currentPlayer: {}
+    };
+
+    this.updateCurrentPlayer = this.updateCurrentPlayer.bind(this)
+  }
+
+  updateCurrentPlayer(player) {
+    console.log(player)
+    this.setState({ currentPlayer: player });
+  }
+
+  componentDidMount() {
+    const URL = 'http://localhost:4000/players';
+    axios
+      .get(URL)
+      .then(response => {
+        const players = response.data
+        this.setState({ players: players });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col s12">Manu</div>
+        </div>
+        <div className="row">
+          <div className="col s3">
+            <PlayerList
+              players={this.state.players}
+              updateCurrentPlayer = {this.updateCurrentPlayer}
+            />
+          </div>
+          <div className="col s9">
+            <PlayerSingle />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            <PlayerForm />
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col s3"><PlayerList /></div>
-        <div className="col s9"><PlayerSingle /></div>
-      </div>
-      <div className="row">
-        <div className="col s12"><PlayerForm /></div>
-      </div>
-      
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/Components/Player/PlayerList.js
+++ src/Components/Player/PlayerList.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const PlayerList = () => {
-  return ( <div>Player List</div>);
-}
- 
-export default PlayerList
+const PlayerList = props => {
+  return (
+    <div>
+      <ul className="collection with-header">
+        <li className="collection-header">
+          <h5>Players</h5>
+        </li>
+        {props.players.map(player => (
+          <a
+            href="#!"
+            className="collection-item"
+            key={player._id}
+            onClick={() => props.updateCurrentPlayer(player)}
+          >
+            {player.firstName} {player.lastName}
+          </a>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PlayerList;
